5cab386b2c7dd6b4c8662436ef69a66a74e5dd47
ignacio.silva <ignaciosilvag3@gmail.com>
2022-08-01 12:48:38 -0300

    Primer COmmir

---
+++ java.js
@@ -0,0 +1,105 @@
+
+
+// function nombreCompleto() {
+//     let nombre = prompt ("Ingrese su nombre")
+//     let apellido = prompt("Ingrese su apellido")
+//     let datosCompletos = "Buenas " + nombre + apellido  
+//     return datosCompletos 
+// }
+//  function nacionalidad(){
+//   let pesoAr = Number(130.68)
+//   let pesoUy = Number(40,55000)
+//   let monto  = prompt("Ingrese la cantidad de dolares que quiere comprar") 
+//   let valor = prompt ("Ingrese su nacionalidad")
+//   if (valor == "Uruguay"){
+//     return (Number( monto * pesoUy) + " Pesos Uruguayos ")
+//   }
+//   else if (valor == "Argentina"){
+//     return ( Number ( monto * pesoAr) +  " Pesos Argentinos ")
+//   }
+//   else return " Error: No teneoms informacion de la moneda de su nacion"
+// }
+
+// console.log(nombreCompleto()+":" + " Usted debe abonar " +  nacionalidad())
+   
+   
+
+
+
+// const usuario1 = {
+//   nombre: "Ignacio Silva", 
+//   edad: "20",
+//   rol: "Estudiante",
+//   cuerso:"Javascript"
+// }
+
+// const usuario2 = {
+//   nombre: "Nicolas Silva", 
+//   edad: "16",
+//   rol: "Estudiante",
+//   cuerso:"Javascript",
+//   tel: {
+//     personal:12345,
+//     trabajo:56789
+//   }
+// }
+
+
+// console.log(usuario1)
+// console.log(usuario2)
+// console.log(usuario2.tel.trabajo)
+
+// const producto ={
+//   id: 13,
+//   nombre: "producto1",
+//   categoria: "zapatillas",
+//   precio: 1200,
+//   desc: "un producto "
+// }
+// console.log(producto.categoria)
+
+// function producto(id, nombre, stock, precio) {
+//   this.id = id
+//   this.nombre = nombre
+//   this.stock = stock
+//   this.precio = precio
+  
+// }
+// const producto1 = new producto(1, "Zapas", 50, 2200)
+// const producto2 = new producto(2, "Buzo", 50, 24200)
+
+// function Mascota(nombre, tipo, edad){
+//   this.nombre = nombre,
+//   this.tipo = tipo,
+//   this.edad = edad
+//   this.sumar = function (){
+//     return edad + 120
+//   }
+// }
+
+// const mascota1 = new Mascota("Ronnie", "perro", 6)
+
+// console.log(mascota1.sumar  )
+
+// class mascota{
+//   constructor (nombre, tipo,edad) {
+//       this.nombre = nombre,
+//       this.tipo = tipo,
+//       this.edad = edad
+//   }
+//   saludara(){
+//     console.log(this.nombre + "te esta saludando!")
+//   }
+//   cunplirAños(){
+//     this.edad++
+// }
+// }
+
+ var a = 5;
+ a=10
+ console.log(a)
+
+ const b = 15
+  
+ console.log(b)
+
